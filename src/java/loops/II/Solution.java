554bd3a552384cd3ae153f3f58a26364721766da
Sheets <smsheets@iastate.edu>
2019-11-15 16:18:50 -0600

    Loops II

---
+++ src/java/loops/II/Solution.java
@@ -0,0 +1,26 @@
+package java.loops.II;
+
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String[] argh) {
+		Scanner in = new Scanner(System.in);
+		int q = in.nextInt();
+		for (int i = 0; i < q; i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int n = in.nextInt();
+			int current = a;
+			for (int j = 0; j < n; j++) {
+				current += ((Math.pow(2, j) * b));
+				if (j == n - 1) {
+					System.out.print(current);
+					continue;
+				}
+				System.out.print(current + " ");
+			}
+			System.out.println();
+		}
+		in.close();
+	}
+}
