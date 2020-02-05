47a6874daa6136f8cc0dde69b28eaece225ef58b
Sheets <smsheets@iastate.edu>
2020-02-05 12:46:02 -0600

    nice

--- src/java/_1DArray/I/Solution.java
+++ src/java/_1DArray/I/Solution.java
@@ -18,8 +18,8 @@ public class Solution {
         scan.close();
 
         // Prints each sequential element in array a
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+        for (int i : a) {
+            System.out.println(i);
         }
     }
 }
